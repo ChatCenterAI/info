7bd64148f0e866f40097c0f65f943990f19dffce
KouIshihara <as.it.were.kou@gmail.com>
2018-09-27 15:33:42 +0900

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Chatcenter Ai Document',
+  description: 'こちらのドキュメントでは、ChatCenter Aiのご登録方法、ご利用開始方法から、詳細設定機能、APIを用いたカスタマイズ開発方法までをご説明いたします。',
+  themeConfig: {
+    sidebar: [
+      '/',
+      //['/NEW', 'NEW.md']
+    ]
+  },
+  base: "/info/",
+}
